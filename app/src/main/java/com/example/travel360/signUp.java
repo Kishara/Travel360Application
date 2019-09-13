c5452764ab2d4251011966a714b3539bcf246e79
Kishara <hathamunagekishara277@gmail.com>
2019-09-13 21:09:30 +0530

    Kishara Sign in/Up interface

---
+++ app/src/main/java/com/example/travel360/signUp.java
@@ -0,0 +1,20 @@
+package com.example.travel360;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class signUp extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+
+    }
+}
