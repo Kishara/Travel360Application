58384eb31e64d47ce193c4b5098518fdaa464f0d
Kishara <hathamunagekishara277@gmail.com>
2019-09-14 15:49:59 +0530

    bus Shedule form - Sachintha

---
+++ app/src/main/java/com/example/travel360/busShedule.java
@@ -0,0 +1,14 @@
+package com.example.travel360;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class busShedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_shedule);
+    }
+}
