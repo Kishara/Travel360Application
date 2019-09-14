3424b502db2edf04073baf5cd015efeaa93dd481
Kishara <hathamunagekishara277@gmail.com>
2019-09-14 15:21:12 +0530

    vehical details form- Sachintha

---
+++ app/src/main/java/com/example/travel360/rentVehicles.java
@@ -0,0 +1,14 @@
+package com.example.travel360;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rentVehicles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent_vehicles);
+    }
+}
