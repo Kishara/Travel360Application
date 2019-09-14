e890d330448a624b95ab063245234cda513f5212
Kishara <hathamunagekishara277@gmail.com>
2019-09-14 15:38:09 +0530

    Train Shedule form - Sachintha

---
+++ app/src/main/java/com/example/travel360/trainShedule.java
@@ -0,0 +1,14 @@
+package com.example.travel360;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trainShedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_train_shedule);
+    }
+}
