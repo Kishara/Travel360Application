97c3cefec9d6c2ca62c3e0d6d182d4c32cdd7946
Kishara <hathamunagekishara277@gmail.com>
2019-09-22 00:09:59 +0530

    User sign up - Kishara

--- app/src/main/java/com/example/travel360/signUp.java
+++ app/src/main/java/com/example/travel360/signUp.java
@@ -1,146 +1,215 @@
 package com.example.travel360;
 
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.squareup.picasso.Picasso;
-
-import com.example.travel360.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class signUp extends AppCompatActivity {
 
 
-    EditText txtfname,txtlname,txtusername,txtcontact,txtpass1,txtpass2;
-    Button signUpbtn;
-    DatabaseReference dbRef;
-    User usr;
+    ImageView ImgUserPhoto;
+    static  int PReqCode = 1;
+    static  int REQUESCODE = 1;
+    Uri pickedImgUri;
 
-    //image upload
-    private static final int PICK_IMAGE_REQUEST = 1;
-    private ImageButton mButtonChooseImage;
-    private ImageView mImageView;
-    private Uri mImageUri;
+    private EditText userEmail,userPassword,userPAssword2,userName,userContact;
+    private ProgressBar loadingProgress;
+    private Button regBtn;
+    private TextView regSignInTxt;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        mImageView = findViewById(R.id.userDp);
-        mButtonChooseImage = findViewById(R.id.addImgBtn);
-        txtfname = findViewById(R.id.Fnametxt);
-        txtlname = findViewById(R.id.Lnametxt);
-        txtusername = findViewById(R.id.Usernametxt);
-        txtcontact = findViewById(R.id.Contacttxt);
-        txtpass1 = findViewById(R.id.Pass1txt);
-        txtpass2 = findViewById(R.id.Pass2txt);
-        signUpbtn = findViewById(R.id.Create);
-        usr = new User();
+        userEmail = findViewById(R.id.regMail);
+        userPassword = findViewById(R.id.regPassword);
+        userPAssword2 = findViewById(R.id.regPassword2);
+        userName = findViewById(R.id.regName);
+        userContact = findViewById(R.id.regContact);
+        loadingProgress = findViewById(R.id.regProgressBar);
+        regBtn = findViewById(R.id.regBtn);
+        regSignInTxt = findViewById(R.id.regSignIn);
+        loadingProgress.setVisibility(View.INVISIBLE);
 
+        mAuth = FirebaseAuth.getInstance();
 
-        //image upload
-        mButtonChooseImage.setOnClickListener(new View.OnClickListener() {
+        regSignInTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                openFileChooser();
-            }
-        });
-
-
-        //data insert to firebase
-        signUpbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dbRef = FirebaseDatabase.getInstance().getReference().child("User");
-
-                String userFname = txtfname.getText().toString();
-                String userLname = txtlname.getText().toString();
-                String userUsername = txtusername.getText().toString();
-                String userpass1 =  txtpass1.getText().toString();
-                String userpass2 =  txtpass2.getText().toString();
-                try {
-
-
-                    usr.setFname(userFname.trim());
-                    usr.setLname(userLname.trim());
-                    usr.setUsername(userUsername.trim());
-                    usr.setContact(Integer.parseInt(txtcontact.getText().toString().trim()));
-                    usr.setPass1(userpass1.trim());
-                    usr.setPass2(userpass2.trim());
-
-                    dbRef.push().setValue(usr);
-
-                    Toast.makeText(getApplicationContext(),"Welcome to Travel 360!",Toast.LENGTH_SHORT).show();
-                    clearControls();
-
-                }
-                catch (NumberFormatException ex1){
-                    Toast.makeText(getApplicationContext(),"Invalid Contact No!",Toast.LENGTH_SHORT).show();
-                }
-                openProfile();
-                }
-            private void openProfile(){
-                Intent intent = new Intent(signUp.this,homePage.class);
+                Intent intent = new Intent(signUp.this,signIn.class);
                 startActivity(intent);
             }
+        });
 
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                regBtn.setVisibility(View.INVISIBLE);
+                loadingProgress.setVisibility(View.VISIBLE);
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+                final String password2 = userPAssword2.getText().toString();
+                final String name = userName.getText().toString();
+                final String contact = userName.getText().toString();
 
+                if (email.isEmpty() || name.isEmpty() || password.isEmpty() || password2.isEmpty() || contact.isEmpty() || !password.equals(password2)){
+
+                    showMessage("Please Verify all fields");
+                    regBtn.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    CreateUserAccount(email,name,password);
+                }
+            }
+        });
+        ImgUserPhoto = findViewById(R.id.regUserPhoto);
+
+        ImgUserPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT<=22){
+                    checkAndRequestForPermission();
+                }
+                else
+                {
+                    openGallery();
+                }
+            }
         });
 
     }
 
-    private void clearControls(){
-        txtfname.setText("");
-        txtlname.setText("");
-        txtusername.setText("");
-        txtcontact.setText("");
-        txtpass1.setText("");
-        txtpass2.setText("");
+    private void CreateUserAccount(String email, final String name, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    showMessage("Account created");
+                    updateUserInfo( name ,pickedImgUri,mAuth.getCurrentUser());
+
+                }
+                else
+                {
+                    showMessage("Account creation failed" + task.getException().getMessage());
+                    regBtn.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 
-    /*private void FileChooser(){
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent,1);
-    }*/
-    private void openFileChooser(){
-        Intent intent = new Intent();
-        intent.setType("Image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    private void updateUserInfo(final String name, Uri pickedImgUri, final FirebaseUser currentUser) {
+
+        StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("users_photos");
+        final StorageReference imageFilePath = mStorage.child(pickedImgUri.getLastPathSegment());
+        imageFilePath.putFile(pickedImgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(name)
+                                .setPhotoUri(uri)
+                                .build();
+
+                        currentUser.updateProfile(profileUpdate)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()){
+                                            showMessage("Register Complete");
+                                            updateUI();
+                                        }
+
+                                    }
+                                });
+                    }
+                });
+            }
+        });
+
     }
+    private void updateUI() {
+
+        Intent homeActivity = new Intent(getApplicationContext(), homePage.class);
+        startActivity(homeActivity);
+        finish();
+    }
+
+    private void showMessage(String message) {
+
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+    }
+
+    private void openGallery() {
+
+        Intent galleryIntent =  new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESCODE);
+    }
+
+    private void checkAndRequestForPermission() {
+
+
+        if (ContextCompat.checkSelfPermission(signUp.this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(signUp.this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Toast.makeText(signUp.this,"Please accept for required permission",Toast.LENGTH_SHORT).show();
+
+            }
+            else{
+                ActivityCompat.requestPermissions(signUp.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},PReqCode);
+            }
+        }
+        else
+            openGallery();
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData()!=null){
-            mImageUri = data.getData();
+        if (resultCode == RESULT_OK && requestCode == REQUESCODE && data != null){
 
-            /*try{
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),mImageUri);
-                mImageView.setImageBitmap(bitmap);
-            }
-            catch (IOException ex2){
-                ex2.printStackTrace();
-            }*/
-            Picasso.with(this).load(mImageUri).into(mImageView);
+
+            pickedImgUri = data.getData();
+            ImgUserPhoto.setImageURI(pickedImgUri);
         }
-
     }
-
 }
