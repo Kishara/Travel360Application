69e99118e9d1d87eca3f2514d1a11c3d4bcce49a
YehanMalith <yehanmalith1234@gmail.com>
2019-09-23 17:56:18 +0530

    new interfaces-yehan and sachintha

---
+++ app/src/main/java/com/example/travel360/ShowDetailsHotels.java
@@ -0,0 +1,14 @@
+package com.example.travel360;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowDetailsHotels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_details_hotels);
+    }
+}
