d26709396cbdbf20637fb500570ad3242e54dfde
YehanMalith <yehanmalith1234@gmail.com>
2019-09-23 15:50:12 +0530

    yehan-insert

--- app/src/main/java/com/example/travel360/model/Hotel.java
+++ app/src/main/java/com/example/travel360/model/Hotel.java
@@ -1,4 +1,40 @@
 package com.example.travel360.model;
 
 public class Hotel {
+
+    private String hoteldate;
+    private String hotelroom;
+    private String hoteladult;
+    private String hotelchild;
+
+    public Hotel() {
+    }
+
+    public String getHoteldate() {
+        return hoteldate;
+    }
+    public void sethoteldate(String hoteldate) {
+        this.hoteldate = hoteldate;
+    }
+
+    public String gethotelroom() {
+        return hotelroom;
+    }
+    public void sethotelroom(String hotelroom) {
+        this.hotelroom = hotelroom;
+    }
+
+    public String gethoteladult() {
+        return hoteladult;
+    }
+    public void sethoteladult(String hoteladult) {
+        this.hoteladult = hoteladult;
+    }
+
+    public String gethotelchild() {
+        return hotelchild;
+    }
+    public void sethotelchild(String hotelchild) {
+        this.hotelchild = hotelchild;
+    }
 }
