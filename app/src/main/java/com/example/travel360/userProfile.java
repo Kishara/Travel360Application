1a281bdc11a1a8c2049ef400d574f6a09b83348f
Kishara <hathamunagekishara277@gmail.com>
2019-09-24 08:43:54 +0530

    User profile Delete & Logout - Kishara

--- app/src/main/java/com/example/travel360/userProfile.java
+++ app/src/main/java/com/example/travel360/userProfile.java
@@ -1,15 +1,22 @@
 package com.example.travel360;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -19,6 +26,9 @@ public class userProfile extends AppCompatActivity {
     FirebaseUser currentUser;
 
     Button editProfileBtn;
+    Button userLogout;
+    Button deleteAccount;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,9 @@ public class userProfile extends AppCompatActivity {
 
         displayProfile();
 
+        userLogout = findViewById(R.id.btnLogout);
+        deleteAccount = findViewById(R.id.btnDeleteAccount);
+        progressBar = findViewById(R.id.progressBar);
         editProfileBtn = findViewById(R.id.editProfile);
 
         editProfileBtn.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +50,58 @@ public class userProfile extends AppCompatActivity {
             public void onClick(View v) {
                 Intent editProfileActivity = new Intent(getApplicationContext(), editProfile.class);
                 startActivity(editProfileActivity);
+            }
+        });
+
+        deleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(userProfile.this);
+                dialog.setTitle("ARe you sure?");
+                dialog .setMessage("delete this account will result in completely removing your account from the system and you won't be able to access the app");
+
+                dialog.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        progressBar.setVisibility(View.VISIBLE);
+                        currentUser.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if(task.isSuccessful()){
+                                    Toast.makeText(userProfile.this,"Account deleted",Toast.LENGTH_LONG).show();
+
+                                    Intent intent = new Intent(userProfile.this,signIn.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                }else {
+                                    Toast.makeText(userProfile.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+                    }
+                });
+                dialog.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                AlertDialog alertDialog = dialog.create();
+                alertDialog.show();
+            }
+        });
+
+        userLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(userProfile.this,signIn.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(),"Logout success",Toast.LENGTH_LONG).show();
             }
         });
 
